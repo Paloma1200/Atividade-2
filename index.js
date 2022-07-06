9f5f39190a1f0532e243d2b9d5a92eecd294fcdc
Paloma <paloma.digital@hotmail.com>
2022-07-05 22:59:57 -0300

    cadastro de eventos

---
+++ index.js
@@ -0,0 +1,33 @@
+let idade = 0;
+let listaParticipante = 0;
+let dataEvento = 0;
+
+const data = new Date()
+
+const dia = String(data.getDate()).padStart(2, '0')
+
+const mes = String(data.getMonth() + 1).padStart(2, '0')
+
+const ano = data.getFullYear()
+
+const dataAtual = `${dia}/${mes}/${ano}`
+
+dataEvento = prompt("Informe a data do evento");
+
+if (dataEvento > dataAtual ) {
+    idade = prompt("Informe a sua idade");
+
+    if (idade >=18){
+        listaParticipante = prompt("Informe quantidade de pessoas");
+
+        if (listaParticipante <= 100) {
+            console.log("Cadastro permitido");
+        } else {
+            console.log("Cadastro não permitido limite excedido");
+        }
+    } else {
+        console.log("Cadastro não permitido pela idade");
+    }
+} else {
+    console.log("Cadastro não permitido data inválida");
+}
